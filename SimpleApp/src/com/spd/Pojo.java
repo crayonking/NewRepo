a9ba2a4ad65dcfc55345fe6fe2a19afc40fbcc8e
crayonking <crayonboss01@gmail.com>
2022-10-14 12:57:03 +0530

    committed here

---
+++ SimpleApp/src/com/spd/Pojo.java
@@ -0,0 +1,34 @@
+package com.spd;
+
+public class Pojo {
+	String name;
+	String role;
+	int id;
+	String market;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMarket() {
+		return market;
+	}
+	public void setMarket(String market) {
+		this.market = market;
+	}
+	
+	
+}
