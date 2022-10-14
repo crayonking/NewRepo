07415305f4eb8b932586dc3c2d5714c1aa63e792
crayonking <64580326+crayonking@users.noreply.github.com>
2022-10-14 12:11:58 +0530

    Create App.java
    Added App.java (Simple class)

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App{
+
+  
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+     System.out.println("enter name");
+    String n=sc.nextLine();
+     System.out.println("hello"+n);
+  }
+}
+    
